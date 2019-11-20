e4ec4b9368b6a4170d48642fd23f95d5f858ee2d
mart1nez94 <armandoomtzz@gmail.com>
2019-11-20 10:50:12 -0600

    scaffold for login module and sing-in component

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SingInComponent } from './sing-in/sing-in.component';
+
+@NgModule({
+  declarations: [SingInComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoginModule { }
