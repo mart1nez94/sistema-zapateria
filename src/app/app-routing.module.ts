14b55a4e4f92be63f2363051a6f816202cec8db7
mart1nez94 <armandoomtzz@gmail.com>
2019-11-20 22:25:39 -0600

    scaffold for register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //Importando Componente "Sing-In"
 import { SingInComponent } from './login/sing-in/sing-in.component';
+//Importando Componente "Register"
+import { RegisterComponent } from './login/register/register.component';
 
 const routes: Routes = [
   {
     path: '', // /
     component: SingInComponent
+  },
+  {
+    path: 'registrar', // /registrar
+    component: RegisterComponent
   }
 ];
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './../app-routing.module';
 import { SingInComponent } from './sing-in/sing-in.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [SingInComponent],
+  declarations: [SingInComponent, RegisterComponent],
   imports: [
     CommonModule,
     AppRoutingModule
